931d2a8eb10533b9e763c62e7b957464859b21e8
github username <github email address>
2020-11-06 01:30:13 -0600

    populates models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+    has_many :reviews
+    has_many :user, through: :reviews
+
+    validates :title, presence: true, uniqueness: true
+    validates :title, length: { maximum: 35 }
+    validates :is_title_case
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :rating, presence: true
+  validates :rating, :inclusion => { :in => 1..5, :message => " should be between 1 to 5" }
+  validates :content, :presence: true, length: { maximum: 130 }
 end
