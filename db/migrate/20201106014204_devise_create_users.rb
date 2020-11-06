dacebfa785be4c13b29f95790f667cc77cb4fd2c
github username <github email address>
2020-11-06 00:28:13 -0600

    fixes rememberable

--- db/migrate/20201106014204_devise_create_users.rb
+++ db/migrate/20201106014204_devise_create_users.rb
@@ -8,12 +8,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :username
 
-      # ## Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-      # ## Rememberable
-      # t.datetime :remember_created_at
+      ## Rememberable
+      t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
