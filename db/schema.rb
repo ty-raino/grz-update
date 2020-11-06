49d1918c731ce58558d0182c23f14abde5b15ebc
github username <github email address>
2020-11-06 01:05:04 -0600

    adds comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_065111) do
+ActiveRecord::Schema.define(version: 2020_11_06_070322) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "review_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -36,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_11_06_065111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
   add_foreign_key "reviews", "games"
   add_foreign_key "reviews", "users"
 end
