b7e81cd0438ff622c7fb25290c82a1771bcb4fa1
github username <github email address>
2020-11-06 19:19:51 -0600

    adds authentication links to navbar

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, :inclusion => { :in => 1..5, :message => " should be between 1 to 5" }
   validates :content, :presence: true, length: { maximum: 130 }
+
+  # sort by good rating and bad rating or filter 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_070322) do
+ActiveRecord::Schema.define(version: 2020_11_06_070606) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_11_06_070322) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["review_id"], name: "index_comments_on_review_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
