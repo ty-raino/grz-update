a2f04d5ce90076285131135e6db2b2262de225cd
github username <github email address>
2020-11-11 00:52:15 -0600

    updates user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    # rescue_from ActiveRecord::RecordNotFound, with: :not_found
   
-    def not_found
-      flash[:error] = "Page not found"
-      redirect_to root_path
-    end
+    # def not_found
+    #   flash[:error] = "Page not found"
+    #   redirect_to root_path
+    # end
 
     protected
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,50 +1,58 @@
 class ReviewsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_reviews, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_reviews, only: [:show, :edit, :update, :destroy]
+  before_action :review_owner, only: [:edit, :update, :destroy]
 
-    def index
-        @reviews = current_user.reviews
+  def index
+    @reviews = Review.all
+  end  
+
+  def show
+  end 
+
+  def new
+    @review = Review.new
+  end
+
+  def create 
+    @review = current_user.reviews.build(reviews_params)
+    if @review.save 
+      redirect_to review_path(@review)
+    else 
+      render :new
     end
+  end   
 
-    def show
-    end 
+  def edit
+  end
 
-    def new
-        @review = Review.new
+  def update 
+    if @review.update(reviews_params)
+      redirect_to review_path(@review)
+    else 
+      render :edit
     end
+  end   
 
-    def create 
-        @review = current_user.reviews.build(reviews_params)
-        if @review.save 
-          redirect_to review_path(@review)
-        else 
-          render :new
-        end
-      end 
-
-    def edit
-    end
-
-    def update 
-        if @review.update(reviews_params)
-          redirect_to review_path(@review)
-        else 
-          render :edit
-        end
-      end 
-
-    def destroy
-        @review.destroy
-        redirect_to reviews_path
-    end
+  def destroy
+    @review.destroy
+    redirect_to reviews_path
+  end
 
 private
 
-    def set_reviews
-        @review = current_user.reviews.find(params[:id])
-    end
+  def set_reviews
+    @review = Review.find(params[:id])
+  end
 
-    def reviews_params
-        params.require(:review).permit(:game_id,:rating, :content, :created_at, :updated_at)
-    end
+  def reviews_params
+    params.require(:review).permit(:game_id, :user_id, :rating, :content, :created_at, :updated_at)
+  end
+  
+  def review_owner
+  unless @review.user_id == current_user.id
+    flash[:error] = 'Access denied'
+    redirect_to reviews_path
+   end
+  end  
 end
