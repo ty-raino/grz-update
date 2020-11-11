7f9e67e35865d3d8c1342b064e92caff4b07e0db
github username <github email address>
2020-11-11 17:57:51 -0600

    updates review show page and model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,15 +3,16 @@ class Review < ApplicationRecord
   belongs_to :game
 
   validates :rating, presence: true
-  validates :rating, :inclusion => { :in => 1..5, :error => " should be between 1 to 5" }
+  validates :rating, :inclusion => { :in => 1..5, :message => " should be between 1 to 5" }
   validates :content, presence: true, length: { maximum: 130 }
 
   # sort by good rating and bad rating or filter 
 
   def posted
-    created_at.strftime("Posted: %m/%e/%y %l:%M %p")
+    created_at.strftime("Posted: %d.%m.%y")
   end
 
   def last_updated
+    updated_at.strftime("Updated at: %d.%m.%y")
   end
 end
