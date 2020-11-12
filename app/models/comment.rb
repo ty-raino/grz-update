dc7f96ed6eb136397361cefb418a4285f12fb43c
github username <github email address>
2020-11-12 11:47:13 -0600

    removes comments

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :review
-
-  validates :content, :presence: true, length: { maximum: 130 }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_040135) do
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "review_id", null: false
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["review_id"], name: "index_comments_on_review_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_11_12_000422) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -52,8 +42,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_040135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "reviews"
-  add_foreign_key "comments", "users"
   add_foreign_key "reviews", "games"
   add_foreign_key "reviews", "users"
 end
