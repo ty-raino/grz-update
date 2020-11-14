5c867e845a9886d95bc6ddef611ee87179e7e34d
github username <github email address>
2020-11-14 01:41:01 -0600

    adds omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
-         :omniauthable, omniauth_providers: [:google_oauth_2]
-
+         :omniauthable, omniauth_providers: [:google_oauth2]
          def self.from_google(uid:, email:, full_name:, avatar_url:)
           if user = User.find_by(email: email)
             user.update(uid: uid, full_name: full_name, avatar_url: avatar_url) unless user.uid.present?
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 
 module GrzUpdate
   class Application < Rails::Application
